5bb62c5b76bbc19d7b717ec252657a41dc73fc7e
庸人自扰、 <elvmx47@aliyun.com>
2019-06-28 16:06:33 +0800

    06-28PM tabbar完善

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,38 @@
 <template>
   <div class="page-home">
     <router-view></router-view>
-    <h1>我是承载着 电影列表页、影院列表页，个人中心页</h1>
+
+    <Tabbar
+      :tabs="[
+      { name: '电影', icon: 'icon-films', to: '/films' },
+      { name: '影院', icon: 'icon-cinemas', to: '/cinemas' },
+      { name: '我的', icon: 'icon-center', to: '/center' },
+    ]"
+    />
   </div>
 </template>
 
 <script>
+import Tabbar from "@/components/Tabbar/index.vue";
+
 export default {
-  name: "home"
+  name: "home",
+
+  components: {
+    Tabbar
+  }
 };
 </script>
+
+<style lang="scss">
+.page-home {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  > div:first-child {
+    flex: 1;
+    overflow-y: auto;
+  }
+}
+</style>
+
